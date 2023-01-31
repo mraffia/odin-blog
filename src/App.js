71c67049db9d57e6582bc187fd37849703b64a3b
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-31 15:44:53 +0700

    Install react-router-dom and add basic App.js structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Nav from './components/Nav.js';
+// import HomePage from './components/HomePage.js';
+// import PostPage from './components/PostPage.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // basename="/odin-blog"
+    <BrowserRouter>
+      <div className="container">
+        {/* <Nav /> */}
+
+        <div className="content">
+          <Routes>
+            {/* <Route path="/" element={<HomePage />} />
+            <Route path="/post" element={<PostPage />} /> */}
+          </Routes>
+        </div>
+
+        <div className="footer">
+          <a href="https://github.com/mraffia"><strong>mraffia</strong>&nbsp;<i class="fa fa-github" style={{ "font-size": "18px" }}></i> </a>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
